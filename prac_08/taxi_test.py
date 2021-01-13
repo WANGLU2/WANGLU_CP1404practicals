2213c924a9a252f6c05d2104e3e72e0aa612c32e
LAPTOP-T0VA7OK3\16933 <wanglu8259@gmail.com>
2021-01-13 18:22:19 +0800

    Class Variables

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -1,7 +1,7 @@
 from prac_08.taxi import Taxi
 
 def main():
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
     print(my_taxi)
     my_taxi.start_fare()
